299ad4d2eb94bc21e868149c2dbcf36bed051d07
Goutham Veeramachaneni <cs14btech11014@iith.ac.in>
2015-04-22 19:57:00 +0530

    Fix wrong login route

--- server/server.js
+++ server/server.js
@@ -92,14 +92,14 @@ app.get('/', function (req, res, next) {
   });
 });
 
-app.get('/auth/account', ensureLoggedIn('/login.html'), function (req, res, next) {
+app.get('/auth/account', ensureLoggedIn('/login'), function (req, res, next) {
   res.render('pages/loginProfiles', {
     user: req.user,
     url: req.url
   });
 });
 
-app.get('/link/account', ensureLoggedIn('/login.html'), function (req, res, next) {
+app.get('/link/account', ensureLoggedIn('/login'), function (req, res, next) {
   res.render('pages/linkedAccounts', {
     user: req.user,
     url: req.url
